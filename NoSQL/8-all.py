9012568d79f36511dc5d43942b480d3230fce2b1
C4lice <laura.aupetit002@gmail.com>
2025-08-15 21:23:46 +0200

    feat to create the file 8-all.py

---
+++ NoSQL/8-all.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+8-all.py
+Module contenant la fonction list_all.
+"""
+
+
+def list_all(mongo_collection):
+    """
+    Liste tous les documents d'une collection.
+    """
+    cursor = mongo_collection.find()
+    return cursor
