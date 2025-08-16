470c6350cad67ac5c42752938299b2d40d46bf61
C4lice <laura.aupetit002@gmail.com>
2025-08-16 12:16:51 +0200

    feat to create the file 10-update_topics.py

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Module containing the function update_topics.
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Changes all topics of a school based on the name.
+    """
+    document = {'name': name}
+    values = {'$set': {'topics': [topic for topic in topics]}}
+    mongo_collection.update_many(document, values)
